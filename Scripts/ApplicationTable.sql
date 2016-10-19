8179ad1fe16966d98286ec2ecf5692f6e263bf1b
Daniel <johnson512n28@Hotmail.com>
2016-10-19 18:55:48 -0400

    InitialUpload

---
+++ Scripts/ApplicationTable.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Application]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(250) NULL, 
+    [DeveloperId] INT NOT NULL, 
+    [ReleaseDate] DATE NULL, 
+    [Device] INT NOT NULL
+)
